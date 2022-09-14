d8fd6de8e76c5a7769bcdd5ad645879670b6591f
David Klingler <dklingle@c2r4s3.42lausanne.ch>
2022-09-14 16:21:50 +0200

    First upload

---
+++ C_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dklingle <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/31 14:43:25 by dklingle          #+#    #+#             */
+/*   Updated: 2022/08/31 14:49:24 by dklingle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
